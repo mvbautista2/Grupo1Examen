5964d74343317fc0a8140b7c57fb1807bbdc5a75
baromero6 <baromero6@espe.edu.ec>
2021-08-04 20:41:00 -0500

    Configuracion Swagger

--- src/main/java/ec/edu/espe/distribuidas/Grupo1/model/Persona.java
+++ src/main/java/ec/edu/espe/distribuidas/Grupo1/model/Persona.java
@@ -10,20 +10,15 @@
  */
 package ec.edu.espe.distribuidas.Grupo1.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
